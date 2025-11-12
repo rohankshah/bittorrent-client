9e0456ae3d61e497e4eb12cd5611ce369cdaea2e
Rohan Shah <moreinmore@gmail.com>
2025-11-12 22:03:38 +0530

    encode info to SHA1, incorrect as of now

--- index.js
+++ index.js
@@ -1,69 +1,19 @@
 const fs = require('node:fs');
+const { parseMetadata } = require('./utils/parseMetaData');
+const { encode } = require('./utils/encode');
+const crypto = require('crypto');
+
 try {
     const data = fs.readFileSync('./test2.torrent')
-    const res = parseMetadata(data, 0)
-    console.log(res)
+    const res = parseMetadata(data)
+
+    const bencoded = encode(res['value']['info'])
+
+    const hash = crypto.createHash('sha1');
+    hash.update(bencoded)
+    const hashed = hash.digest('hex')
+    console.log(hashed)
 
 } catch (err) {
     console.error(err);
-}
-
-
-function parseMetadata(data, i = 0) {
-    let res = []
-
-    while (i < data.length) {
-        let current = data.toString('ascii', i, i + 1)
-
-        if (current === 'e') {
-            return { value: res, index: i + 1 }
-        }
-
-        if (current === 'd') {
-            let obj = {}
-            i += 1
-            while (data.toString('ascii', i, i + 1) !== 'e') {
-                const { value: key, index: keyIndex } = parseMetadata(data, i)
-                i = keyIndex
-
-                const { value: val, index: valIndex } = parseMetadata(data, i)
-                i = valIndex
-
-                obj[key] = val
-            }
-            i += 1
-            return { value: obj, index: i }
-        }
-
-        if (current === 'l') {
-            let res = []
-            i += 1
-            while (data.toString('ascii', i, i + 1) !== 'e') {
-                const { value: output, index } = parseMetadata(data, i)
-                res.push(output)
-                i = index
-            }
-            i += 1
-            return { value: res, index: i }
-        }
-
-        if (current === 'i') {
-            let j = i
-            while (data[j] !== 0x65) {
-                j += 1
-            }
-            const num = parseInt(data.toString('ascii', i + 1, j))
-            return { value: num, index: j + 1 }
-        }
-
-        let j = i
-        while (data[j] !== 0x3A) {
-            j += 1
-        }
-        const length = parseInt(data.toString('ascii', i, j))
-        const subStr = data.toString('ascii', j + 1, j + 1 + length)
-        return { value: subStr, index: j + 1 + length }
-    }
-
-    return { value: res, index: i }
 }
