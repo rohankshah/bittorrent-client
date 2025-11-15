95545e3a5e50da03302b7466f6e0920d07b46a69
Rohan Shah <moreinmore@gmail.com>
2025-11-15 11:09:56 +0530

    send announce request

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ try {
     const data = fs.readFileSync('./test2.torrent')
     const res = bencode.decode(data)
 
+    const totalFileLength = res['info']['files']?.reduce((total, curr) => total + curr?.length, 0)
+
     // Get tracker url
     const trackerUrl = bufferToEncoding(res['announce'], 'utf8')
     const hostName = new URL(trackerUrl).hostname
@@ -22,8 +24,8 @@ try {
 
     const serverPort = 6969;
 
-    const udpServer = new UDP_Protocol(serverAddress, serverPort)
-    udpServer.connectMessage()
+    const udpServer = new UDP_Protocol(serverAddress, serverPort, infoHash, totalFileLength)
+    udpServer.connectRequest()
 
 
     setInterval(() => { }, 1000)
--- udp_protocol.js
+++ udp_protocol.js
@@ -1,9 +1,13 @@
 import dgram from 'node:dgram'
+import { generateRandomString } from './lib/utils.js'
 
 export class UDP_Protocol {
-    constructor(serverAddress, serverPort) {
+    constructor(serverAddress, serverPort, infoHash, totalFileLength) {
         this.serverAddress = serverAddress
         this.serverPort = serverPort
+        this.infoHash = infoHash
+        this.peerId = generateRandomString(20)
+        this.totalFileLength = totalFileLength
 
         this.server = dgram.createSocket('udp4');
 
@@ -19,30 +23,74 @@ export class UDP_Protocol {
 
     sendPacket(buf) {
         this.server.send(buf, 0, buf.length, this.serverPort, this.serverAddress, (err) => {
-            if (err) console.error('Send error:', err);
-            else console.log('Connect request sent');
+            if (err) {
+                console.log('Send error:', err)
+            }
+            else {
+                console.log('Request sent');
+            }
         });
     }
 
     recieveMessageCallback = (res, rinfo) => {
-        console.log('res', res?.byteLength, rinfo)
+        console.log('res', res, rinfo)
 
-        const action = res.subarray(0, 5)?.readUInt32BE()
-        const transactionId = res.subarray(4, 9)?.readUInt32BE()
-        const connectionId = res.subarray(8, 17)?.readBigUInt64BE()
+        if (res.byteLength >= 20) {
+            console.log('announce response returned')
+        }
 
-        if (transactionId === this.transactionId && action === 0) {
-            this.connectionId = connectionId
+        if (res.byteLength >= 16) {
+            const action = res.subarray(0, 5)?.readUInt32BE()
+            const transactionId = res.subarray(4, 9)?.readUInt32BE()
+            const connectionId = res.subarray(8, 17)?.readBigUInt64BE()
+
+            if (action === 0 && transactionId === this.transactionId) {
+                console.log('is connect Response')
+                this.connectionId = connectionId
+                this.announceRequest()
+            }
         }
     }
 
-    connectMessage() {
+    connectRequest() {
         // Create connect packet
         const buf = Buffer.alloc(16)
 
         buf.writeBigUInt64BE(0x41727101980n, 0)
         buf.writeUInt32BE(0, 8)
         buf.writeUInt32BE(this.transactionId, 12);
+
+        this.sendPacket(buf)
+    }
+
+    announceRequest() {
+        this.resetTransactionId()
+
+        const buf = Buffer.alloc(98)
+        // Connection Id
+        buf.writeBigUInt64BE(BigInt(this.connectionId), 0)
+        // Action - Announce (1)
+        buf.writeUInt32BE(1, 8)
+        // Info hash
+        buf.write(this.infoHash, 16)
+        // Peer Id
+        buf.write(this.peerId, 36)
+        // Downloaded
+        buf.writeBigUInt64BE(BigInt(0), 56)
+        // Left
+        buf.writeBigUInt64BE(BigInt(this.totalFileLength), 64)
+        // Uploaded
+        buf.writeBigUInt64BE(BigInt(0), 72)
+        // Event - None (0)
+        buf.writeUInt32BE(0, 80)
+        // IP address - (0)
+        buf.writeUInt32BE(0, 84)
+        // key - Optional not important atm
+        buf.writeUInt32BE(79012, 88)
+        // num_want - (-1) default
+        buf.writeUInt32BE(50, 92)
+        // port - 6881 rn
+        buf.writeUInt16BE(6881, 96)
 
         this.sendPacket(buf)
     }
