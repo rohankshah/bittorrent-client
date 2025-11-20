846e859f3e3e0c96050a0b818375920921b6a4fb
Rohan Shah <moreinmore@gmail.com>
2025-11-20 10:06:03 +0530

    read bitfield

--- peer_protocol.js
+++ peer_protocol.js
@@ -12,6 +12,17 @@ export class Peer_Protocol {
 
         this.savedBuffer = Buffer.alloc(0)
         this.handShakeReceived = false
+
+
+        this.socket.on('data', (data) => this.listenData(data))
+
+        this.socket.on('error', (err) => {
+            // console.log(`Error connecting to ${ip}:${gggggg}:`, err.message)
+        })
+
+        this.socket.on('close', () => {
+            // console.log(`Connection closed: ${ip}:${port}`)
+        })
     }
 
     createHandshakeBuffer() {
@@ -36,49 +47,60 @@ export class Peer_Protocol {
             console.log(`Connected to ${this.host}:${this.port}`)
             this.socket.write(buf)
         })
-
-        this.socket.on('data', (data) => {
-
-            // TCP doesn't return all data altogether. So need to first collect all of it
-            this.savedBuffer = Buffer.concat([this.savedBuffer, data])
-
-            while (this.savedBuffer.byteLength >= 4 && !this.handShakeReceived && this.savedBuffer.byteLength >= 68) {
-                const tempData = this.savedBuffer?.subarray(0, 68)
-                this.savedBuffer = this.savedBuffer.subarray(68)
-
-                // const len = tempData?.readUInt8(0)
-                // const messageId = tempData?.subarray(1, 20)?.toString('ascii')
-                // const reserved = tempData?.subarray(20, 28)?.readUint32BE()
-                const infoHash = tempData?.subarray(28, 48)?.toString('hex')
-                // const peerId = tempData?.subarray(48, 68)?.toString()
-
-
-                // Only proceed if...
-                if (infoHash === this.infoHash) {
-                    console.log('received handshake')
-                    this.handShakeReceived = true
-                }
-            }
-
-            while (this.savedBuffer.byteLength >= 4 && this.handShakeReceived && this.savedBuffer.byteLength >= this.savedBuffer?.readUInt32BE(0) + 4) {
-                const tempData = this.savedBuffer?.subarray(0, 68)
-                this.savedBuffer = this.savedBuffer.subarray(68)
-
-                const length = tempData.readUInt32BE(0)
-                const messageId = tempData.readUInt8(4)
-                const payload = tempData.subarray(5)
-
-                console.log(length, messageId, payload)
-            }
-        })
-
-        this.socket.on('error', (err) => {
-            // console.log(`Error connecting to ${ip}:${port}:`, err.message)
-        })
-
-        this.socket.on('close', () => {
-            // console.log(`Connection closed: ${ip}:${port}`)
-        })
     }
 
+    listenData(data) {
+        // TCP doesn't return all data altogether. So need to first collect all of it
+        this.savedBuffer = Buffer.concat([this.savedBuffer, data])
+
+        while (this.savedBuffer.byteLength >= 4 && !this.handShakeReceived && this.savedBuffer.byteLength >= 68) {
+            const tempData = this.savedBuffer?.subarray(0, 68)
+            this.savedBuffer = this.savedBuffer.subarray(68)
+
+            // const len = tempData?.readUInt8(0)
+            // const messageId = tempData?.subarray(1, 20)?.toString('ascii')
+            // const reserved = tempData?.subarray(20, 28)?.readUint32BE()
+            const infoHash = tempData?.subarray(28, 48)?.toString('hex')
+            // const peerId = tempData?.subarray(48, 68)?.toString()
+
+
+            // Only proceed if...
+            if (infoHash === this.infoHash) {
+                console.log('received handshake')
+                this.handShakeReceived = true
+            }
+        }
+
+        while (this.savedBuffer.byteLength >= 4 && this.handShakeReceived && this.savedBuffer.byteLength >= this.savedBuffer?.readUInt32BE(0) + 4) {
+            const tempData = this.savedBuffer?.subarray(0, 68)
+            this.savedBuffer = this.savedBuffer.subarray(68)
+
+            const length = tempData.readUInt32BE(0)
+            const messageId = tempData.readUInt8(4)
+            const payload = tempData.subarray(5)
+
+            switch (parseInt(messageId)) {
+                case 5:
+                    this.handleBitfield(payload)
+                    break
+            }
+        }
+    }
+
+    handleBitfield(payload) {
+        // We read bitfield from right to left
+        // The first byte of the bitfield corresponds to indices 0 - 7 from high bit to low bit, respectively. - from the spec
+        for (let i = 0; i < payload.length; i++) {
+            let byte = payload[i]
+            for (let j = 0; j < 8; j++) {
+                // If byte is 1
+                if (byte % 2) {
+                    const pieceIndice = i * 8 + (7 - j)
+                    // console.log('pieceIndice', pieceIndice)
+                }
+                // Removes last bit from byte. So next iteration 2nd last bit is the last one
+                byte = Math.floor(byte / 2)
+            }
+        }
+    }
 }
